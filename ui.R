39dfb87e276f07b26562c268f9a265243449d2bb
Winston Chang <winston@stdout.org>
2014-06-21 09:28:17 -0500

    Remove help popups

--- ui.R
+++ ui.R
@@ -8,61 +8,6 @@ actionLink <- function(inputId, ...) {
          ...)
 }
 
-helpPopup <- function(title, content, placement = "top", trigger = "hover") {
-
-  if (!(placement %in% c('right', 'top', 'left', 'bottom'))) {
-    stop("placement must be one of 'right', 'top', 'left', or 'bottom'.")
-  }
-  if (!(trigger %in% c('click', 'hover', 'focus', 'manual'))) {
-    stop("trigger must be one of 'click', 'hover', 'focus', or 'manual'.")
-  }
-
-  tagList(
-    singleton(
-      tags$head(
-        tags$script("$(function() { $(\"[data-toggle='popover']\").popover(); })")
-      )
-    ),
-    tags$a(
-      href = "#",
-      class = "btn btn-link",
-      `data-toggle` = "popover",
-      title = title,
-      `data-content` = content,
-      `data-animation` = TRUE,
-      `data-placement` = placement,
-      `data-trigger` = trigger,
-
-      tags$i(class="icon-question-sign")
-    )
-  )
-}
-
-helpTooltip <- function(text, placement = "top") {
-
-  if (!(placement %in% c('right', 'top', 'left', 'bottom'))) {
-    stop("placement must be one of 'right', 'top', 'left', or 'bottom'.")
-  }
-
-  tagList(
-    singleton(
-      tags$head(
-        # This part needs to be
-        tags$script("$(function() { $(\"[data-toggle='tooltip']\").tooltip(); })")
-      )
-    ),
-    tags$a(
-      href = "#",
-      class = "btn btn-link",
-      `data-toggle` = "tooltip",
-      `data-placement` = placement,
-      title = text,
-
-      tags$i(class="icon-question-sign")
-    )
-  )
-}
-
 shinyUI(fluidPage(
   titlePanel("Movie explorer"),
   fluidRow(
@@ -71,8 +16,6 @@ shinyUI(fluidPage(
         h4("Filter"),
         sliderInput("reviews", "Minimum number of reviews on Rotten Tomatoes",
           10, 300, 80, step = 10),
-        helpPopup("help title", "help content"),
-        helpTooltip("help content"),
         sliderInput("year", "Year released", 1940, 2014, value = c(1970, 2014)),
         sliderInput("oscars", "Minimum number of Oscar wins (all categories)",
           0, 4, 0, step = 1),
